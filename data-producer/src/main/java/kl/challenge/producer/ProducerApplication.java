c6f68c72aa6ee95533894bdebabc1241069865ab
kelvinleong <kelvin_liang885@hotmail.com>
2018-07-23 14:00:01 +0800

    -add logback.xml -add logger info -application.properties enhancement

--- data-producer/src/main/java/kl/challenge/producer/ProducerApplication.java
+++ data-producer/src/main/java/kl/challenge/producer/ProducerApplication.java
@@ -1,5 +1,7 @@
 package kl.challenge.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,7 +9,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ProducerApplication {
+	private static Logger logger = LoggerFactory.getLogger(ProducerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
+		logger.info("============Data Producer Start Success=====================");
 	}
 }
